b554eb856f06b232e5147e4a57a7190b7a990c84
Tseganeh Z. Gichamo <zacctsega@yahoo.com>
2025-09-27 10:16:08 -0400

    Update namelist_mod.f90

--- namelist_mod.f90
+++ namelist_mod.f90
@@ -13,7 +13,7 @@ module namelist_mod
     character*256      :: tile_restart_path = ""
     character*256      :: output_path = ""
     character*256      :: static_filename = ""
-    character*3        :: gfsv17 = ""
+    logical            :: gfsv17
     character*3        :: lndp_layout = ""
     character*256      :: lndp_input_file = ""
     character*256      :: lndp_output_file = ""
@@ -35,7 +35,7 @@ contains
     character*256       :: tile_restart_path
     character*256       :: output_path
     character*256       :: static_filename
-    character*3         :: gfsv17
+    logical             :: gfsv17
     character*3         :: lndp_layout
     character*256       :: lndp_input_file
     character*256       :: lndp_output_file
@@ -48,7 +48,7 @@ contains
                             lndp_layout, lndp_input_file, lndp_output_file, lndp_var_list, n_var_lndp
 
     lndp_var_list = 'XXX'
-    gfsv17 = "NO"
+    gfsv17 = .false.
 
     open(30, file=namelist%namelist_name, form="formatted")
      read(30, run_setup)
@@ -63,6 +63,7 @@ contains
     namelist%tile_restart_path   = tile_restart_path
     namelist%output_path         = output_path
     namelist%static_filename     = static_filename
+    namelist%gfsv17              = gfsv17
 
     namelist%lndp_layout         = lndp_layout
     namelist%lndp_input_file     = lndp_input_file
