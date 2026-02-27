3beca9c0dfd698de598705908f0caea9ef63b00f
tsga <zacctsega@yahoo.com>
2026-02-27 00:44:49 +0000

    clean up

--- vector2tile_restart_mod.f90
+++ vector2tile_restart_mod.f90
@@ -859,7 +859,6 @@ contains
     status = nf90_put_var(ncid, varid , tile%swe(:,:,itile)   , &
       start = (/1,1,1/), count = (/namelist%tile_size, namelist%tile_size, 1/))
     
-    swe_snd_land = tile%swe(:,:,itile) !identical to sheleg
     status = nf90_inq_varid(ncid, "weasdl", varid)
     status = nf90_put_var(ncid, varid , tile%swe(:,:,itile)  , & 
       start = (/1,1,1/), count = (/namelist%tile_size, namelist%tile_size, 1/))
@@ -868,7 +867,6 @@ contains
     status = nf90_put_var(ncid, varid , tile%snow_depth(:,:,itile)   , &
       start = (/1,1,1/), count = (/namelist%tile_size, namelist%tile_size, 1/))
   
-    swe_snd_land = tile%snow_depth(:,:,itile) !identical to snwdph
     status = nf90_inq_varid(ncid, "snodl", varid)
     status = nf90_put_var(ncid, varid ,  tile%snow_depth(:,:,itile)   , &
       start = (/1,1,1/), count = (/namelist%tile_size, namelist%tile_size, 1/))
