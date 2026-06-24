5b933a09eaf27032147677843f34994376cc7123
Tseganeh Z. Gichamo <zacctsega@yahoo.com>
2026-06-24 09:46:20 -0400

    fix time var writes

--- vector2tile_restart_mod.f90
+++ vector2tile_restart_mod.f90
@@ -1046,12 +1046,13 @@ contains
 
     !TODO: only working with fhr=6. Better way to get these time info?
     !prev_date "2024-06-09 12:00:00" !fhr=006  !date="2024-06-09_18:00:00" !time_iso_str="2024-06-09T18:00:00Z"
-    if (len_trim(namelist%prev_date) < 13) then
+    if (len_trim(namelist%prev_date) < 19) then
         print *, 'ERROR: write_s3history requires prev_date in namelist (YYYY-MM-DD_HH:MM:SS)'
         stop 10
     end if
-    write(time_iso_str, '(a10,a1,a2,a7)') date(1:10), "T", date(12:13), ":00:00Z"
-    write(hr_since_str, '(a10,a1,a2,a6)') date(1:10), " ", date(12:13), ":00:00"
+    write(time_iso_str, '(a10,a1,a2,a1,a2,a1,a2,a1)') date(1:10), 'T', date(12:13), ':', date(15:16), ':', date(18:19), 'Z'
+    write(hr_since_str, '(a10,a1,a2,a1,a2,a1,a2)') namelist%prev_date(1:10), ' ', namelist%prev_date(12:13), ':', &
+                                                   namelist%prev_date(15:16), ':', namelist%prev_date(18:19)
 
     csg_filename=trim(namelist%output_path)//"/"//trim(namelist%s3h_runtype)//".t"//namelist%prev_date(12:13)//"z.csg_sfc.f006.nc"
     print*, "Creating history file: ", trim(csg_filename)
@@ -1062,9 +1063,13 @@ contains
     if (status /= nf90_noerr) call handle_err(status)
     status = nf90_redef(ncid)
     if (status /= nf90_noerr) call handle_err(status)
-    status = nf90_put_att(ncid, varid, 'units', 'hours since '//hr_since_str)  !hours since 2024-06-09 12:00:00') 
+    status = nf90_put_att(ncid, varid, 'units', 'hours since '//hr_since_str)  
     if (status /= nf90_noerr) call handle_err(status)
     status = nf90_enddef(ncid)
+    if (status /= nf90_noerr) call handle_err(status)
+
+    ! put fhr6
+    status = nf90_put_var(ncid, varid, (/6.0_8/))
     if (status /= nf90_noerr) call handle_err(status)
   
     !only dimension vars time_iso and lat/lon not written in s3history_header()
